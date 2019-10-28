a710c880515f226cb6023fae2a7bd85ab8a22e91
ShaniaK <wardshania18@gmail.com>
2019-10-28 14:52:25 -0400

    Made changes to my challenge

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // 1: Set the text of the <h1> element
-document.querySelector("h1").textContent = "Bucket List"
+let title = document.querySelector("h1");
+title.innerHTML = "My Bucket List"
 
 // 2: Set the color of the <h1> to a different color
 title.style.color = "red";
@@ -7,20 +8,34 @@ title.style.color = "red";
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
 const desc = document.querySelector(".desc");
-desc.append("the quick brown fox jump over the lazy one")
+desc.append("Hello");
+
 
 // 4: Set the class of the <ul> to 'list'
+document.querySelector("ul").className = "list";
 
 
 // 5: Create a new list item and add it to the <ul>
-
+function my function(){
+var listitem = document.createElement('LI');
+    listitem.innerHTML= "<input> Free Dive"
+    document.querySelector('.List').appendChild(ListItem);
+}
 
 // 6: Change all <input> elements from text fields to checkboxes
-
-
+    let inputTags = document.querySelectorAll('input')
+    for(let index = 0; index < inputTags.length; index++) {
+        const element = inputTags[index].setAttribute('type','checkbox');
+    }
 
 // 7: Create a <button> element, and set its text to 'Delete'
 // Add the <button> inside the '.extra' <div>
-
+var btn = document.createElement("Button");
+btn.innerHTML = "Delete";
+let extra = document.querySelector('.extra')
+extra.appendChild(btn);
 
 // 8: Remove the '.extra' <div> element from the DOM when a user clicks the 'Delete' button
+btn.addEventListener('click', () => {
+extra.remove();
+})
